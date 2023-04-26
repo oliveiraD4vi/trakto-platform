5e7113d0edc61bc6ed2caee5c82b73b9f7128434
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-04-25 22:20:43 -0300

    feat: Deleted unnecessary component

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "../components/home/home.component";
 import { DashboardComponent } from "../components/dashboard/dashboard.component";
 
 const routes: Routes = [
@@ -8,11 +7,6 @@ const routes: Routes = [
     path: "",
     pathMatch: "full",
     component: DashboardComponent,
-  },
-  {
-    path: "home",
-    pathMatch: "full",
-    component: HomeComponent,
   },
 ];
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
-import { HomeComponent } from "../components/home/home.component";
 import { DashboardComponent } from "../components/dashboard/dashboard.component";
 import { HeaderComponent } from "../components/header/header.component";
 import { DropdownComponent } from "../components/dropdown/dropdown.component";
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    DashboardComponent,
-    HeaderComponent,
-    DropdownComponent,
-  ],
+  declarations: [DashboardComponent, HeaderComponent, DropdownComponent],
   imports: [CommonModule, DashboardRoutingModule],
 })
 export class DashboardModule {}
