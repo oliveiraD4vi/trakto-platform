0741b19b09373ffb2e2b673f6577b61d6e1fb154
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-04-27 01:26:27 -0300

    feat: Added correct date change by week

--- src/app/dashboard/courseware/courseware.component.ts
+++ src/app/dashboard/courseware/courseware.component.ts
@@ -11,4 +11,35 @@ export class CoursewareComponent {
     total_per_page: 10,
     order_orientation: "desc",
   };
+
+  startDate = new Date();
+  endDate = new Date();
+  oneWeek = 7 * 24 * 60 * 60 * 1000; // 1 week in ms
+
+  constructor() {
+    this.endDate.setTime(this.startDate.getTime() + this.oneWeek);
+  }
+
+  nextWeeks() {
+    this.startDate.setTime(this.startDate.getTime() + this.oneWeek);
+    this.filterOptions.created_between_start = this.startDate;
+    this.endDate.setTime(this.endDate.getTime() + this.oneWeek);
+    this.filterOptions.created_between_end = this.endDate;
+  }
+
+  previousWeeks() {
+    this.startDate.setTime(this.startDate.getTime() - this.oneWeek);
+    this.filterOptions.created_between_start = this.startDate;
+    this.endDate.setTime(this.endDate.getTime() - this.oneWeek);
+    this.filterOptions.created_between_end = this.endDate;
+  }
+
+  formatDate(date: Date): string {
+    const formattedDate = date.toLocaleDateString(undefined, {
+      day: "2-digit",
+      month: "2-digit",
+    });
+    const [day, month] = formattedDate.split("/");
+    return `${month}/${day}`;
+  }
 }
